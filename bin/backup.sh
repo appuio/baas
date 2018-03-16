2541fde517e914fe47b6c608804dcfc992d0416c
Tobias Brunner <tobias@tobru.ch>
2018-03-16 16:28:13 +0100

    dev

--- bin/backup.sh
+++ bin/backup.sh
@@ -1,18 +1,32 @@
 #!/bin/sh
 
 if [ -z ${NAMESPACE} ]; then
-  echo "NAMESPACE is not defined"
+  echo "[FATAL] NAMESPACE is not defined"
   exit 1
 fi
 
-echo "executing restic backup in namespace ${NAMESPACE}"
+echo "[INFO] - [${NAMESPACE}] - storing volumes ${VOLUMES}"
 
 restic backup \
   --hostname ${NAMESPACE} \
-  --one-file-system \
+  --tag volumes
   /data && \
-echo "[INFO] listing available snapshots" && \
+
+echo "[INFO] - [${NAMESPACE}] - listing available snapshots" && \
 restic snapshots \
   --no-lock \
   --host ${NAMESPACE} && \
+
+echo "[INFO] - [${NAMESPACE}] - notifying monitoring" && \
 echo curl -fsS --retry 3 https://hchk.io/
+
+
+echo "[INFO] - [${NAMESPACE}] - storing databases"
+
+# Find databases with oc label filter
+# Connect to them using oc rsh and stdout / stdin redirection
+## restic backup \
+##   --hostname ${NAMESPACE} \
+##   --tag postgres
+##   --stdin \
+##   --stdin-filename postgres
